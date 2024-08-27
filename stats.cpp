8188b0a77bb43a0538b3e34709d010d089658b46
Mayilsamy A <mayilsamy.a@philips.com>
2024-08-27 17:50:59 +0530

    example code

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,12 @@
 #include "stats.h"
+#include <bits/stdc++.h> 
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::<double> statData) {
     //Implement statistics here
+
+    double sum = accumulate(statData.begin(), statData.end(), 0.0);
+
+    double avg = sum / statData.size();
+
+    return avg;
 }
