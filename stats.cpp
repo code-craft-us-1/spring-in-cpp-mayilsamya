e096518b68787059ac4ba23d2f9fd98986006e33
Mayilsamy A <mayilsamy.a@philips.com>
2024-08-27 19:29:46 +0530

    Add fix

--- stats.cpp
+++ stats.cpp
@@ -1,12 +1,18 @@
 #include "stats.h"
 #include <bits/stdc++.h> 
+#include <iostream>
 
-stat Statistics::ComputeStatistics(const std::<double> statData) {
-    //Implement statistics here
 
-    double sum = accumulate(statData.begin(), statData.end(), 0.0);
+stat Statistics::ComputeStatistics(const std::vector<double> statData)
+{
+    int index = 0;
+    double sumOfData = 0;
+    double avg = 0;
 
-    double avg = sum / statData.size();
-
-    return avg;
+    for (index = 0; index <= statData.size(); index++)
+    {
+        sumOfData = sumOfData + statData.at(index);
+    }
+    avg =  sumOfData / statData.size();
+    return stat();
 }
