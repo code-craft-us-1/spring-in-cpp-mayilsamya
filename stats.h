f0be52d581caa7fae607a957d4c74391b3522898
Mayilsamy A <mayilsamy.a@philips.com>
2024-08-27 17:59:54 +0530

    added git ignore

--- stats.h
+++ stats.h
@@ -1,6 +1,6 @@
 #include <vector>
 
-namespace Statistics {
+namespace Stats {
     // define the Stats structure here. See the tests to infer its properties
     
     Stats ComputeStatistics(const std::vector<double> statData );
