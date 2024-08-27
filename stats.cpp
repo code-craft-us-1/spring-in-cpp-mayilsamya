0f053a2244e2ea6898665ab60ff810d39f50c813
Mayilsamy A <mayilsamy.a@philips.com>
2024-08-27 19:03:23 +0530

    added git ignore

--- stats.cpp
+++ stats.cpp
@@ -1,7 +1,7 @@
 #include "stats.h"
 #include <bits/stdc++.h> 
 
-Stats Statistics::ComputeStatistics(const std::<double> statData) {
+stat Statistics::ComputeStatistics(const std::<double> statData) {
     //Implement statistics here
 
     double sum = accumulate(statData.begin(), statData.end(), 0.0);
--- stats.h
+++ stats.h
@@ -1,7 +1,7 @@
 #include <vector>
 
-namespace Stats {
+namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
     
-    Stats ComputeStatistics(const std::vector<double> statData );
+    stat ComputeStatistics(const std::vector<double> statData );
 }
